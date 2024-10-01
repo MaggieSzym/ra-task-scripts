0eb3548f86c2eac0d5131b509e3e8a2281a1dc51
MaggieSzym <42606965+MaggieSzym@users.noreply.github.com>
2024-10-01 20:50:11 +0100

    Update batch_executable.bat

--- batch_executable.bat
+++ batch_executable.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Activate the Anaconda environment
-call C:\Path\To\Anaconda\Scripts\activate.bat markerlessTrackingEnv
+call C:\Path\To\Anaconda\Scripts\activate.bat <markerless tracking env>
 REM Run your Python script in a new minimized window
 start /b python C:\Path\To\Current\Directory\MT_GUI.py
