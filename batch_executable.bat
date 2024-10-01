500c2506e90414b125cb63b1df154b657a59cdf0
MaggieSzym <42606965+MaggieSzym@users.noreply.github.com>
2024-10-01 18:45:54 +0100

    Add batch_executable.bat

---
+++ batch_executable.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Activate the Anaconda environment
+call C:\Path\To\Anaconda\Scripts\activate.bat markerlessTrackingEnv
+REM Run your Python script in a new minimized window
+start /b python C:\Path\To\Current\Directory\MT_GUI.py
